0e133245b5272e235f1d08dcf65634b7f3f13c52
farisramadhan13 <farisramadhan345@gmail.com>
2024-08-20 15:55:09 +0700

    update

--- src/pages/PortfolioDetailPage.js
+++ src/pages/PortfolioDetailPage.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import Navbar from '../components/Navbar';
 
 const PortfolioDetailPage = () => {
-  const { id } = useParams();
+  const { category, id } = useParams();
+  // Anda bisa menggunakan id untuk mendapatkan detail portofolio yang spesifik dari API atau state
 
   return (
     <div className="bg-dark-bg text-light-text min-h-screen p-8">
-      <h2 className="text-2xl font-bold">Portfolio Item {id}</h2>
-      {/* Add detailed content for the portfolio item here */}
+      <Navbar />
+      <h2 className="text-2xl font-bold mb-4">Portfolio {id} in {category.replace('-', ' ').toUpperCase()}</h2>
+      {/* Tambahkan detail konten untuk portofolio */}
     </div>
   );
 };
