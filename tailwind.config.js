4428f5eeb047f32500b0cd2946107822754c3919
farisramadhan13 <farisramadhan345@gmail.com>
2024-08-20 15:57:16 +0700

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,22 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'dark-bg': '#121212',
+        'glass-bg': 'rgba(255, 255, 255, 0.1)', // Glass effect
+        'light-text': '#ffffff',
+        'glow': '#00ff99', // Glow effect color
+      },
+      boxShadow: {
+        'glow': '0 0 10px rgba(0, 255, 255, 0.7)', // Glow effect shadow
+      },
+      backdropBlur: {
+        'sm': '4px', // Glass effect
+      },
+    },
   },
   plugins: [],
 }
