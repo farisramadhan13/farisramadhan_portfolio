cd535b3ad198604fb146ec5e49c7eadedecd357a
farisramadhan13 <farisramadhan345@gmail.com>
2024-08-20 16:03:07 +0700

    update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,9 +4,9 @@ import { Link } from 'react-router-dom';
 const Navbar = () => {
   return (
     <nav className="bg-dark-bg text-light-text p-4 glass shadow-glow">
-      <ul className="flex justify-center space-x-4">
-        <li><Link to="/" className="hover:text-glow">Home</Link></li>
-        <li><Link to="/portfolio" className="hover:text-glow">Portfolio</Link></li>
+      <ul className="flex justify-center space-x-6">
+        <li><Link to="/" className="text-2xl font-bold hover:text-glow">Home</Link></li>
+        <li><Link to="/portfolio" className="text-2xl font-bold hover:text-glow">Portfolio</Link></li>
       </ul>
     </nav>
   );
